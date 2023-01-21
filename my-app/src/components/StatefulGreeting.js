28ba7525910a0723409c8207fabd34aba770ec21
JoeQuigley1 <joeyjtq@hotmail.com>
2023-01-21 14:20:14 +0000

    finish updating state tutorial

--- my-app/src/components/StatefulGreeting.js
+++ my-app/src/components/StatefulGreeting.js
@@ -7,28 +7,40 @@ class StatefulGreeting extends React.Component {
         this.state ={
             introduction: "Hello",
             buttonText: "Exit",
+            count : 0
         };
     }
     handleClick() {
-        this.setState({
-            introduction: this.state.introduction === "Hello" ? "Goodbye!" : "Hello",
-            buttonText: this.state.buttonText === "Enter" ? "Exit" : "Enter"
-        }, () => {
-            // Needs a callback function to make sure that the state will change //
-        console.log('new state', this.state.introduction)
-        console.log('new state', this.state.buttonText)
-        });
-        console.log(this.state.introduction)
-        console.log(this.state.buttonText)
+        this.setState((prevState, prevProps) => {
+            console.log('Previous State:', prevState)
+            console.log('Previous Props:', prevProps)
+            return {
+                introduction: prevState.introduction === "Hello" ? "Goodbye" : "Hello",
+                buttonText: prevState.buttonText === "Exit" ? "Enter" : "Exit"
+            }
+        })
     }
+    increment() {
+        this.setState((prevState, prevProps) => {
+            console.log('Previous State:', prevState)
+            console.log('Previous Props:', prevProps)
+            return {
+                count: prevState.count + 1
+            }
+        })
+    }
+
     render(){
         return (
             <div>
                 <h1>{this.state.introduction} {this.props.name}! {this.props.greeting}</h1>
                 <button onClick={() => this.handleClick()}> {this.state.buttonText}</button>
+                <button onClick={() => this.increment()}> Increment </button>
+                <p> You have clicked {this.state.count} times.
+                </p>
             </div>
         )
     }
 }
 
-export default StatefulGreeting;
+export default StatefulGreeting
